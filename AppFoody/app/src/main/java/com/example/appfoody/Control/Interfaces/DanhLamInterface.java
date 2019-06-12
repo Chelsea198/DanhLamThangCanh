a4916c9683a67d774fed35b603802ea9d424abcd
DESKTOP-J2U6JA8\Chelsea _198 <phantrongba1998@gmail.com>
2019-06-12 14:31:22 +0700

    Chill

--- AppFoody/app/src/main/java/com/example/appfoody/Control/Interfaces/DanhLamInterface.java
+++
@@ -1,7 +0,0 @@
-package com.example.appfoody.Control.Interfaces;
-
-import com.example.appfoody.Model.DanhLamThangCanhModel;
-
-public interface DanhLamInterface {
-    void getDanhSachDanhLamThangCanh(DanhLamThangCanhModel danhLamThangCanhModel);
-}
